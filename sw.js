76725a25a5af116f3c0ecedcf944b2627ec80b66
Anthony Ng <newyork.anthonyng@gmail.com>
2018-10-30 20:24:50 -0400

    Add service worker

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "hbfs-mta-cache-v1";
+const urlsToCache = [
+   "/",
+   "/index.html",
+   "/mta.js"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log("Opened cache");
+                return cache.addAll(urlsToCache);
+            })
+    )
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    )
+
+});
