a9efeef8178d981de97d7313a70553e182a0c4d6
Anthony Ng <newyork.anthonyng@gmail.com>
2018-10-31 08:39:46 -0400

    Cache MTA routes

--- sw.js
+++ sw.js
@@ -9,7 +9,6 @@ self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => {
-                console.log("Opened cache");
                 return cache.addAll(urlsToCache);
             })
     )
@@ -19,11 +18,30 @@ self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => {
+                // Cache hit - return response
                 if (response) {
                     return response;
                 }
-                return fetch(event.request);
+
+                // Otherwise, make new fetch request and cache the response
+                const fetchRequest = event.request.clone();
+
+                return fetch(fetchRequest)
+                    .then(response => {
+                        // Don't cache invalid responses
+                        if (!response || response.status !== 200) {
+                            return response;
+                        }
+
+                        const responseToCache = response.clone();
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                console.log(`ServiceWorker: caching request for ${response.url}`)
+                                cache.put(event.request, responseToCache);
+                            });
+
+                        return response;
+                    });
             })
     )
-
 });
