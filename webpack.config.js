833d01601b702e7ec605f4bba4cf014b9e6e918b
Anthony Ng <newyork.anthonyng@gmail.com>
2018-11-07 20:53:21 -0500

    Add workbox-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 const path = require("path");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const CleanPlugin = require("clean-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 const filesToCopy = [
     "./index.html",
-    "./sw.js",
     "./subway_map.pdf"
 ];
+
+const SUBWAY_STATION_URL = "http://traintimelb-367443097.us-east-1.elb.amazonaws.com/getStationsByLine";
+const REAL_TIME_URL = "http://traintimelb-367443097.us-east-1.elb.amazonaws.com/getTime";
+
+const DIST_DIRECTORY = "dist";
 
 module.exports = {
     entry: path.resolve(__dirname, "src/index.js"),
@@ -17,7 +22,22 @@ module.exports = {
     },
 
     plugins: [
-        new CleanWebpackPlugin(["dist"]),
-        new CopyWebpackPlugin(filesToCopy)
+        new CleanPlugin([DIST_DIRECTORY]),
+        new CopyPlugin(filesToCopy),
+        new WorkboxPlugin.GenerateSW({
+            swDest: "sw.js",
+            clientsClaim: true,
+            skipWaiting: true,
+            runtimeCaching: [
+                {
+                    urlPattern: new RegExp(REAL_TIME_URL),
+                    handler: "staleWhileRevalidate"
+                },
+                {
+                    urlPattern: new RegExp(SUBWAY_STATION_URL),
+                    handler: "staleWhileRevalidate"
+                }
+            ]
+        })
     ]
 }
