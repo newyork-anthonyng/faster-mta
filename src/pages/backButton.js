290fd7f1a6cfa5877459d7266d39bb738d49c002
Anthony Ng <newyork.anthonyng@gmail.com>
2018-11-22 09:02:43 -0500

    Add backButton styling

--- src/pages/backButton.js
+++ src/pages/backButton.js
@@ -1,6 +1,9 @@
+const linkStyle = "display: inline-block; margin-bottom: 16px;";
+
 function renderBackButton(href) {
   const backEle = document.createElement("a");
   backEle.setAttribute("href", href);
+  backEle.setAttribute("style", linkStyle);
 
   const backText = document.createTextNode("back");
   backEle.appendChild(backText);
